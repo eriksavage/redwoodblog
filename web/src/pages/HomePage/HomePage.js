9bf8867d6b8b39a10b74cbecd050bac9ba35d168
erik savage <erik.k.savage@gmail.com>
2022-10-18 19:14:59 -0700

    adds ArticlesCell and Posts model [D

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,11 +1,14 @@
 import { MetaTags } from '@redwoodjs/web'
 
+import ArticlesCell from 'src/components/ArticlesCell'
+
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
       <h2>Home Page</h2>
       <p>Home Page content!</p>
+      <ArticlesCell />
     </>
   )
 }
